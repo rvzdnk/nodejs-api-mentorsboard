a5246694c60b9affd4e9cbba7da2753b1c8d9983
rvzdnk <rafal93zdunek@gmail.com>
2023-01-06 17:34:02 +0100

    confi passport.js & jwt

---
+++ config/passport.js
@@ -0,0 +1,25 @@
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+const ExtractJWT = passportJWT.ExtractJwt;
+const { Strategy } = passportJWT;
+
+require("dotenv").config();
+const SECRET = process.env.SECRET_KEY;
+const User = require("../models/userSchema");
+
+const params = {
+  secretOrKey: SECRET,
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+};
+
+passport.use(
+  new Strategy(params, async (payload, done) => {
+    try {
+      const user = await User.findOne({ _id: payload.id });
+
+      return user ? done(null, user) : done(new Error("User not found"));
+    } catch (err) {
+      done(err);
+    }
+  })
+);
