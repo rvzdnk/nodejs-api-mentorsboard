c42370b2c188b08002b155867d2bd3cf5c9d98fe
rvzdnk <rafal93zdunek@gmail.com>
2023-01-15 17:23:18 +0100

    bugfix/error-jwt-strategy

--- config/passport.js
+++ config/passport.js
@@ -1,19 +1,18 @@
-const passport = require("passport");
-const passportJWT = require("passport-jwt");
+const passport = require('passport');
 const User = require("../models/userSchema");
-require('dotenv').config({ path: '../.env' });
-const SECRET_KEY = process.env.SECRET_KEY;
 
-const ExtractJWT = passportJWT.ExtractJwt;
-const Strategy = passportJWT.Strategy;
+const JWTStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+
+require('dotenv').config();
 
 const params = {
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: SECRET_KEY,
+  secretOrKey: process.env.SECRET_KEY,
 };
 
 passport.use(
-  new Strategy(params, async (payload, done) => {
+  new JWTStrategy(params, async (payload, done) => {
     try {
       const user = await User.findOne({ _id: payload.id });
 
